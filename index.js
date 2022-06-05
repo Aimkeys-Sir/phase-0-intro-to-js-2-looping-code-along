1f5dd88a23b345814603ba988f35d1ad07955592
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-05 11:24:51 +0300

    commit tree ebb27fac0b2f63e67d99eb1fe5baa5f255cedd62

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let new_array=[];
+const _names=["Guadalupe", "Ollie", "Aki"]
+function writeCards(_names, event="surprise")
+{
+    for(let i=0;i<_names.length;i++)
+    {
+        new_array.push(`Thank you, ${_names[i]}, for the wonderful ${event} gift!`);
+    }
+    return new_array;
+}
+
+function countDown(_number=10)
+{
+    while(_number>-1)
+    {
+        console.log(_number);
+        _number-=1;
+    }
+}
